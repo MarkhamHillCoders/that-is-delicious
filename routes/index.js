e0c000af28e9cd5f366a4eca57c7193ccb05a176
Daniel <github@profoundideation.com>
2017-12-08 23:03:40 -0500

    60096
    60096

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  // res.send('Hey! It Works!');
+  res.send('Hey! It Works!');
   // res.json(req.query);
-  res.render("hello");
+  // res.render("hello");
 });
 
 router.get('reverse/:name', (req, res) =>{
