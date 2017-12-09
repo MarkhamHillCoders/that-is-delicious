6b52d7ef3c03afd325c81e9e2b443765b9a7459f
Daniel <github@profoundideation.com>
2017-12-08 22:53:36 -0500

    update render
    update render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It Works!');
+  // res.send('Hey! It Works!');
+  // res.json(req.query);
+  res.render("hello");
 });
 
 module.exports = router;
